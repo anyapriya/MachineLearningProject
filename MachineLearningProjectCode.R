291ca0fe05efc2d950af5e51346c7856680bc48d
anyapriya <41804916+anyapriya@users.noreply.github.com>
2018-12-06 13:34:00 +0000

    Add files via upload

--- MachineLearningProjectCode.R
+++ MachineLearningProjectCode.R
@@ -2,72 +2,121 @@ library(matrixcalc)
 library(MASS)
 
 
-set.seed(1234)
-
-
-x <- data.frame("Int" = rep(1,100), "Var1" = rnorm(100,10,5), "Var2" = rnorm(100,40,1), "Var3" = rnorm(100,70,20))
-beta <- matrix(c(4,5,-16,0.5), ncol = 1)
-
-y <- as.matrix(x) %*% beta
 
 
 
 
-
-
-#Train
-
-#Validate - tune hy
-
-#Test - evaluate 
+#covMatrix <- diag(corrMatrix) * corrMatrix * diag(corrMatrix)
+#covMatrix
+#cov2cor(covMatrix)
 
 
 
-
-sigmaMatrix <- matrix(rep(0,(8*8)), ncol = 8)
-for(i in 1:8){
-  for(j in 1:8){
-    sigmaMatrix[i,j] <- 0.5^abs(i-j)
+Simulation <- function(times, obs, test.valid.size, p, rho, errorSD, betaVals){
+  Output <- NULL
+  for(i in 1:times){
+    Output <-rbind(Output, Testing(n = obs, sizes = test.valid.size, p = p, rho = rho, errorSD = errorSD, betaVals = betaVals))
   }
+  return(Output)
 }
 
-mysample <- mvrnorm(n = 240, mu = rep(0,8), Sigma = sigmaMatrix)
 
-beta <- matrix(c(3,1.5,0,0,2,0,0,0), ncol = 1)
-error <- rnorm(240)
-sigma <- 3
-y <- mysample %*% beta + sigma*error
+#Called by Simulation, runs one round of the simulation, returns the number of non-zero coefficients and MSE from optimal lambda that minimizes MSE
+Testing <- function(n, sizes, p, rho, errorSD, betaVals){
+  SampleOut <- mySample(n, p, rho, errorSD, betaVals)
+  x <- SampleOut[[1]]
+  y <- SampleOut[[2]]
+  Output <- Validation(x = x, y = y, sizes = sizes)
+  NumbNonZeros <- sum(Output[[1]] != 0)
+  MSE <- Output[[2]]
+  return(c(NumbNonZeros, MSE))
+}
+
+        #Called by Testing, randomly generates the data set
+        mySample <- function(n, p, rho, errorSD, betaVals){
+          SigmaMat <- covMatrix(rho, p)
+          mu <- rep(0,p)
+          x <- mvrnorm(n = n, mu = mu, Sigma = SigmaMat)
+          error <- rnorm(n)
+          y <- x %*% betaVals + errorSD*error
+          return(list(x,y))
+        }
+        
+                  #Called by mySample, creates variance-covariance matrix
+                  covMatrix <- function(rho, p){
+                    covMatrix <- matrix(rep(0,(p^2)), ncol = p)
+                    for(i in 1:p){
+                      for(j in 1:p){
+                        covMatrix[i,j] <- rho^abs(i-j)
+                      }
+                    }
+                    return(covMatrix)
+                  }
+
+#Called by Testing, finds the value of lambda to minimize the MSE and returns the BetaHats and MSE
+Validation <- function(x,y,sizes){
+  testSize <- sizes[1]
+  MinMSE <- optimise(MSEValid, c(0,100), x=x, y=y, sizes = sizes)
+  BetaHat <- coordDescAlg.Lasso(MinMSE$minimum, y[1:testSize], x[1:testSize,])
+  MSE <- MinMSE$objective
+  return(list(BetaHat,MSE))
+}
 
 
 
+#Called by Validation, calculates the value of MSE for a given lambda
+MSEValid <- function(lambda, x, y, sizes){
+  testSize <- sizes[1]
+  validSize <- sizes[2]
+  ytrain = y[1:testSize]
+  xtrain = x[1:testSize,]
+  yvalid = y[(testSize+1):(testSize + validSize)]
+  xvalid = x[(testSize+1):(testSize + validSize),]
+  BetaHat <- coordDescAlg.Lasso(lambda, ytrain, xtrain)
+  MSE <- mean((yvalid - xvalid %*% BetaHat)^2)
+  return(MSE)
+}
 
 
 
+#Divide by variance of thing
+
+#Called by MSEValid and Validation, uses the coordinate descent algorithm to calculate beta for the a given lambda
 coordDescAlg.Lasso <- function(lambda, y, x){
   
-  x <- as.matrix(x)
+  x <- as.matrix(scale(x))
+  y <- scale(y)
   converge <- FALSE
   n <- dim(x)[1]
   p <- dim(x)[2]
   BetaHat <- rep(0, p)
   BetaHat.star <- rep(0, p)
-  r <- matrix(rep(NA,n*p), ncol = p)
+  newBetaHat <- rep(0,p)
+  r <- matrix(rep(0,n*p), ncol = p)
   
   while(!converge){
+    # for(j in 1:p){
+    #   r[,j] <- y - x[,-j]%*%BetaHat[-j]
+    #   BetaHat.star[j] <- (1/n)*(t(x[,j])%*%r[,j]) / (t((x[,j])%*%x[,j]))
+    #   newBetaHat[j] <- sign(BetaHat.star[j])*pmax((abs(BetaHat.star[j]) - lambda / (t((x[,j])%*%x[,j]))), 0)
+    #   
+    # }
     for(j in 1:p){
-      r[,j] <- y - rowSums(x[,-j]*BetaHat[-j])
+      r[,j] <- y - x[,-j]%*%BetaHat[-j]
     }
-    
     BetaHat.star <- (1/n)*colSums(x*r)
     
     newBetaHat <- sign(BetaHat.star)*pmax((abs(BetaHat.star) - lambda), 0)
     
-    if(all(BetaHat == newBetaHat)){
-      converge <- TRUE
-    }
-    else{
-      BetaHat <- newBetaHat
-    }
+
+      if(all(round(BetaHat, 5) == round(newBetaHat,5))){
+        converge <- TRUE
+        break
+      } else {
+        BetaHat <- newBetaHat
+      }
+      
+    
   }
   
   return(BetaHat)
@@ -75,50 +124,66 @@ coordDescAlg.Lasso <- function(lambda, y, x){
 }
 
 
+set.seed(3)
+placeholder <- 0
 
-
-coordDescAlg.Lasso(0.5, y, mysample)
-
-
-
-OptimalLambda <- optim(0.5, coordDescAlg.Lasso, y = input.y, x = input.x)
-
-
-
-
+for(i in 1:100){
+output <- mySample(240, 8, 0.5, 3, betaVals = matrix(c(3,1.5,0,0,2,0,0,0)))
+coordDescAlg.Lasso(0.1, output[[2]][1:20], output[[1]][1:20,])
+}
+placeholder <- placeholder + 1
 
 
 
+ErrorOutput <- output
+x <- ErrorOutput[[1]][1:20,]
+y <- ErrorOutput[[2]][1:20]
+lambda <- 0
 
 
 
 
 
-# 
-# coordDescAlg.ElasNet <- function(y, x, lambda){
-#   
-#   converge <- FALSE
-#   n <- dim(x)[1]
-#   p <- dim(x)[2]
-#   BetaHat <- rep(0, p)
-#   BetaHat.star <- rep(0, p)
-#   r <- matrix(rep(NA,n*p), ncol = p)
-#   
-#   while(!converge){
-#     for(j in 1:p){
-#       r[,j] <- y - rowSums(x[,-j]*BetaHat[-j])
-#     }
-#     
-#     BetaHat.star <- (1/n)*colSums(hadamard.prod(x,r))
-#     
-#     newBetaHat <- sign(BetaHat.star)*pmax((abs(BetaHat.star) - lambda), 0)
-#     
-#     if(all(BetaHat == newBetaHat)){
-#       converge <- TRUE
-#     }
-#     else{
-#       BetaHat <- newBetaHat
-#     }
-#   }
-#   
-# }
+
+set.seed(3)
+
+Simulation(times = 50, obs = 240, test.valid.size = c(20,20), p = 8, rho = 0.5, errorSD = 3, betaVals = matrix(c(3,1.5,0,0,2,0,0,0), ncol = 1))
+
+
+#Don't need to standardize the data
+
+
+
+
+
+coordDescAlg.ElasticNet <- function(lambda, y, x){
+  
+  x <- as.matrix(x)
+  converge <- FALSE
+  n <- dim(x)[1]
+  p <- dim(x)[2]
+  BetaHat <- rep(0, p)
+  BetaHat.star <- rep(0, p)
+  r <- matrix(rep(0,n*p), ncol = p)
+  
+  while(!converge){
+    for(j in 1:p){
+      r[,j] <- y - x[,-j]%*%BetaHat[-j]
+    }
+    BetaHat.star <- (1/n)*colSums(x*r)
+    
+    newBetaHat <- sign(BetaHat.star)*pmax((abs(BetaHat.star) - lambda), 0)
+    
+    if(all(round(BetaHat, 3) == round(newBetaHat,3))){
+      converge <- TRUE
+      break
+    } else {
+      BetaHat <- newBetaHat
+    }
+    
+    
+  }
+  
+  return(BetaHat)
+  
+}
