762c9ab8a5de29979c50fa786b06a28d845b1c63
anyapriya <41804916+anyapriya@users.noreply.github.com>
2018-12-06 15:20:41 +0000

    Updated Project Code

--- MachineLearningProjectCode.R
+++ MachineLearningProjectCode.R
@@ -27,9 +27,11 @@ Testing <- function(n, sizes, p, rho, errorSD, betaVals){
   x <- SampleOut[[1]]
   y <- SampleOut[[2]]
   Output <- Validation(x = x, y = y, sizes = sizes)
-  NumbNonZeros <- sum(Output[[1]] != 0)
-  MSE <- Output[[2]]
-  return(c(NumbNonZeros, MSE))
+  NumbNonZeros.Lasso <- sum(Output[[1]] != 0)
+  NumbNonZeros.ElNet <- sum(Output[[3]] != 0)
+  MSE.Lasso <- Output[[2]]
+  MSE.ElNet <- Output[[4]]
+  return(c(NumbNonZeros.Lasso, MSE.Lasso, NumbNonZeros.ElNet, MSE.ElNet))
 }
 
         #Called by Testing, randomly generates the data set
@@ -56,16 +58,20 @@ Testing <- function(n, sizes, p, rho, errorSD, betaVals){
 #Called by Testing, finds the value of lambda to minimize the MSE and returns the BetaHats and MSE
 Validation <- function(x,y,sizes){
   testSize <- sizes[1]
-  MinMSE <- optimise(MSEValid, c(0,100), x=x, y=y, sizes = sizes)
-  BetaHat <- coordDescAlg.Lasso(MinMSE$minimum, y[1:testSize], x[1:testSize,])
+  MinMSE.Lasso <- optim(c(0.0001), MSEValid.Lasso,  x = x, y = y, sizes = sizes, method = "Brent", lower = 0, upper = 5)
+  BetaHat.Lasso <- coordDescAlg.Lasso(MinMSE.Lasso$minimum, y[1:testSize], x[1:testSize,])
+  
+  MinMSE.ElasticNet <- optim(c(0.0001, 0.0001), MSEValid.ElasticNet,  x = x, y = y, sizes = sizes)
+  BetaHat.ElasticNet <- coordDescAlg.ElasticNet(MinMSE.ElasticNet$par, y[1:testSize], x[1:testSize,])
+  
   MSE <- MinMSE$objective
-  return(list(BetaHat,MSE))
+  return(list(BetaHat.Lasso,MinMSE.Lasso$objective, BetaHat.ElasticNet, MinMSE.ElasticNet$value))
 }
 
 
 
 #Called by Validation, calculates the value of MSE for a given lambda
-MSEValid <- function(lambda, x, y, sizes){
+MSEValid.Lasso <- function(lambda, x, y, sizes){
   testSize <- sizes[1]
   validSize <- sizes[2]
   ytrain = y[1:testSize]
@@ -78,14 +84,26 @@ MSEValid <- function(lambda, x, y, sizes){
 }
 
 
+#Called by Validation, calculates the value of MSE for a given lambda
+MSEValid.ElasticNet <- function(lambda, x, y, sizes){
+  testSize <- sizes[1]
+  validSize <- sizes[2]
+  ytrain = y[1:testSize]
+  xtrain = x[1:testSize,]
+  yvalid = y[(testSize+1):(testSize + validSize)]
+  xvalid = x[(testSize+1):(testSize + validSize),]
+  BetaHat <- coordDescAlg.ElasticNet(lambda, ytrain, xtrain)
+  MSE <- mean((yvalid - xvalid %*% BetaHat)^2)
+  return(MSE)
+}
 
-#Divide by variance of thing
+
+
 
 #Called by MSEValid and Validation, uses the coordinate descent algorithm to calculate beta for the a given lambda
 coordDescAlg.Lasso <- function(lambda, y, x){
   
-  x <- as.matrix(scale(x))
-  y <- scale(y)
+  x <- as.matrix(x)
   converge <- FALSE
   n <- dim(x)[1]
   p <- dim(x)[2]
@@ -95,28 +113,23 @@ coordDescAlg.Lasso <- function(lambda, y, x){
   r <- matrix(rep(0,n*p), ncol = p)
   
   while(!converge){
-    # for(j in 1:p){
-    #   r[,j] <- y - x[,-j]%*%BetaHat[-j]
-    #   BetaHat.star[j] <- (1/n)*(t(x[,j])%*%r[,j]) / (t((x[,j])%*%x[,j]))
-    #   newBetaHat[j] <- sign(BetaHat.star[j])*pmax((abs(BetaHat.star[j]) - lambda / (t((x[,j])%*%x[,j]))), 0)
-    #   
-    # }
     for(j in 1:p){
       r[,j] <- y - x[,-j]%*%BetaHat[-j]
-    }
-    BetaHat.star <- (1/n)*colSums(x*r)
     
-    newBetaHat <- sign(BetaHat.star)*pmax((abs(BetaHat.star) - lambda), 0)
+      BetaHat.star[j] <- (1/n)*(t(x[,j])%*% r[,j]) / (t(x[,j])%*% x[,j])
+      newBetaHat[j] <- sign(BetaHat.star[j])*pmax(BetaHat.star[j] - (lambda / (t(x[,j])%*% x[,j])), 0)
     
-
-      if(all(round(BetaHat, 5) == round(newBetaHat,5))){
+      if(all(round(BetaHat, 3) == round(newBetaHat,3))){
         converge <- TRUE
         break
       } else {
         BetaHat <- newBetaHat
       }
+
       
+    }
     
+
   }
   
   return(BetaHat)
@@ -124,33 +137,26 @@ coordDescAlg.Lasso <- function(lambda, y, x){
 }
 
 
-set.seed(3)
-placeholder <- 0
-
-for(i in 1:100){
-output <- mySample(240, 8, 0.5, 3, betaVals = matrix(c(3,1.5,0,0,2,0,0,0)))
-coordDescAlg.Lasso(0.1, output[[2]][1:20], output[[1]][1:20,])
-}
-placeholder <- placeholder + 1
-
-
-
-ErrorOutput <- output
-x <- ErrorOutput[[1]][1:20,]
-y <- ErrorOutput[[2]][1:20]
-lambda <- 0
 
 
 
 
 
 
-set.seed(3)
-
-Simulation(times = 50, obs = 240, test.valid.size = c(20,20), p = 8, rho = 0.5, errorSD = 3, betaVals = matrix(c(3,1.5,0,0,2,0,0,0), ncol = 1))
 
 
-#Don't need to standardize the data
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -164,26 +170,35 @@ coordDescAlg.ElasticNet <- function(lambda, y, x){
   p <- dim(x)[2]
   BetaHat <- rep(0, p)
   BetaHat.star <- rep(0, p)
+  newBetaHat <- rep(0,p)
   r <- matrix(rep(0,n*p), ncol = p)
   
   while(!converge){
     for(j in 1:p){
       r[,j] <- y - x[,-j]%*%BetaHat[-j]
+      
+      BetaHat.star[j] <- (1/n)*(t(x[,j])%*% r[,j]) / (t(x[,j])%*% x[,j])
+      newBetaHat[j] <- (sign(BetaHat.star[j])*pmax(BetaHat.star[j] - (lambda[1] / (t(x[,j])%*% x[,j])), 0)) / ((1 + 2*lambda[2] ))
+      
+      
+      if(all(round(BetaHat, 5) == round(newBetaHat,5))){
+        converge <- TRUE
+        break
+      } else {
+        BetaHat <- newBetaHat
+      }
+      
     }
-    BetaHat.star <- (1/n)*colSums(x*r)
-    
-    newBetaHat <- sign(BetaHat.star)*pmax((abs(BetaHat.star) - lambda), 0)
-    
-    if(all(round(BetaHat, 3) == round(newBetaHat,3))){
-      converge <- TRUE
-      break
-    } else {
-      BetaHat <- newBetaHat
-    }
-    
-    
   }
   
   return(BetaHat)
   
 }
+
+
+
+set.seed(3)
+
+Simulation(times = 3, obs = 240, test.valid.size = c(20,20), p = 8, rho = 0.5, errorSD = 3, betaVals = matrix(c(3,1.5,0,0,2,0,0,0), ncol = 1))
+
+
